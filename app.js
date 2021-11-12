8018852da236a005723537cd545274044b254423
mandelmonkey <chrismoss411@gmail.com>
2021-11-12 10:38:31 +0000

    improved logging

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const seedPhrase = "REPLACE WITH YOUR PHRASE i.e. sock dog guitar...";
-const addressToFind = "ENTER THE ADDRESS TO FIND i.e. 1GvRme7Lfsj2NEJmwrVEhgAjtjHpeGbf2h5";
-const depth = 50;
+const addressToFind = "REPLACE WITH THE ADDRESS TO FIND 113k8AxTHGGhaQWbrYhRijzynSyyoqF7Np";
+const depth = 10;
 
 var Bip39 = require('bip39');
 const Bip32 = require('bip32');
@@ -20,15 +20,15 @@ for(var i = 0;i<depth;i++){
     for(var i3 = 0;i3<depth;i3++){
       const child = root.deriveHardened(rootIndex).deriveHardened(i).deriveHardened(i2).derive(i3);
       const address = getAddress(child);
-      if(address === addressToFind ){
-        process.stdout.write("\n");
-        process.stdout.write("\n");
-        console.log("found WIF is:"+child.toWIF());
-        return;
-      }
       progress++;
       const percentComplete = (progress / (depth ** 3) * 100).toFixed(2);
       process.stdout.write("searching 1/2 " +percentComplete+"% - "+rootIndex+"'/"+ i+"'/"+i2+"'/"+ i3+" address - "+address+"\r");
+      if(address === addressToFind ){
+        process.stdout.write("\n");
+        process.stdout.write("\n");
+        console.log("found WIF is: "+child.toWIF());
+        return;
+      }
     }
   }
 
@@ -45,16 +45,17 @@ for(var i = 0;i<depth;i++){
       for(var i4 = 0;i4<depth;i4++){
         const child = root.deriveHardened(rootIndex).deriveHardened(i).deriveHardened(i2).deriveHardened(i3).derive(i4);
         const address = getAddress(child);
-        if(address === addressToFind ){
-          process.stdout.write("\n");
-          process.stdout.write("\n");
-          console.log("found WIF is:"+child.toWIF());
-          return;
-        }
+       
         progress++;
         const percentComplete = (progress / (depth ** 4) * 100).toFixed(2);
 
         process.stdout.write("searching 2/2 " +percentComplete+"% - "+rootIndex+"'/"+ i+"'/"+i2+"'/"+ i3+"'/"+i4+" address - "+address+"\r");
+        if(address === addressToFind ){
+          process.stdout.write("\n");
+          process.stdout.write("\n");
+          console.log("found WIF is: "+child.toWIF());
+          return;
+        }
       }
     }
   }
